07fef5c398f052638e3f976ed784a04f0c04459f
Pradeep <p76667@gmail.com>
2023-06-05 15:04:13 +0530

    final commit 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,28 @@
+// Task 1: Create a file Main.java in package com.driver
 package com.driver;
 
 public class Main {
-  public static void main(String[] args){
-      RWOnly rwOnly = new RWOnly();
-      rwOnly.setName("Pradeep");
 
-    String name = rwOnly.getName();
-      System.out.println(name);
-  }
+    // Task 3: create main function in Main.java
+    public static void main (String[] args) throws Exception {
+
+        // Task 3: create an obj of class RWOnly
+        RWOnly rwOnly = new RWOnly();
+
+        // Task 4: Setting a value to name, by directly accessing it using obj and also try printing it Error: you might seen some error, please not it down in commnets
+        // rwnOly.name = "Amit";
+        // System.out.println(rwOnly.name);
+
+        // Exception in thread "main" java.lang.Error: Unresolved compilation problems:
+        // The field RWOnly.name is not visible
+        // The field RWOnly.name is not visible
+        // at com.driver.Main.main(Main.java:10)
+
+        // Task 6: Now, trying to set a value to name in Main function using setter function
+        rwOnly.setName("Pradeep");
+
+        // Task 6: Now, trying to access it's value using getter function
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,12 +1,18 @@
+// Task 1: Create a file RWOnly.java in package com.driver
 package com.driver;
 
+// Task 2: create a class named RWOnly in RWOnly.java 
 public class RWOnly {
+
+    // Task 2: with a private member variable name
     private String name;
 
+    // Task 5: implementing getter member function in class RWOnly
     public String getName() {
         return name;
     }
 
+    // Task 5: implementing setters member function in class RWOnly
     public void setName(String name) {
         this.name = name;
     }
